63156254f0e3df1ca5525c4108859eeebd3dfe5f
Алексей Шмырёв <alxshmrv@gmail.com>
2025-11-22 12:32:25 +0500

    ~

---
+++ Bookify.Domain/Apartments/ValueObjects/Money.cs
@@ -0,0 +1,16 @@
+﻿namespace Bookify.Domain.Apartments.ValueObjects;
+
+public record Money(decimal Amount, Currency Currency)
+{
+    public static Money operator +(Money first, Money second)
+    {
+        if (first.Currency != second.Currency)
+        {
+            throw new InvalidOperationException("Currency does not match");
+        }
+        
+        return new Money(first.Amount + second.Amount, first.Currency);
+    }
+
+    public static Money Zero() => new(0, Currency.None);
+}
