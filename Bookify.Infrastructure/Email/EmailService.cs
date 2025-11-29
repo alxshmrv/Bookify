ff04105803897332eac062f2accf2e2856b8ca22
Алексей Шмырёв <alxshmrv@gmail.com>
2025-11-29 12:16:35 +0500

    Создан проект инфраструктуры и добаленые некоторые реализации интерфейсов Application Layer

---
+++ Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Application.Abstractions.Email;
+
+namespace Bookify.Infrastructure.Email;
+
+internal sealed class EmailService : IEmailService
+{
+    public Task SendAsync(Domain.Users.ValueObjects.Email recipient, string subject, string body, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
