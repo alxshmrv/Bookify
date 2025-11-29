c34c9ff0072548917e8fee2910654f4372ab5824
Алексей Шмырёв <alxshmrv@gmail.com>
2025-11-29 12:37:50 +0500

    Добавление DbContext и кофигураций для сущностей с определением отношений

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
