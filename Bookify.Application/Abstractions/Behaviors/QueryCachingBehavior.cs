9d5c799697f06271df26bd48f753c09b4e51cc21
Алексей Шмырёв <alxshmrv@gmail.com>
2026-01-16 17:24:54 +0500

    ADD Redis Cache + CACHING EXAMPLE

---
+++ Bookify.Application/Abstractions/Behaviors/QueryCachingBehavior.cs
@@ -0,0 +1,52 @@
+﻿using Bookify.Application.Abstractions.Caching;
+using Bookify.Domain.Abstractions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Bookify.Application.Abstractions.Behaviors;
+
+internal sealed class QueryCachingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICachedQuery
+    where TResponse : Result
+{
+    private readonly ICacheService _cacheService;
+    private readonly ILogger<QueryCachingBehavior<TRequest, TResponse>> _logger;
+
+    public QueryCachingBehavior(
+        ILogger<QueryCachingBehavior<TRequest, TResponse>> logger,
+        ICacheService cacheService)
+    {
+        _logger = logger;
+        _cacheService = cacheService;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        var cachedResult = await _cacheService.GetAsync<TResponse>(
+            request.CacheKey,
+            cancellationToken);
+        
+        var name = typeof(TRequest).Name;
+        if (cachedResult is not null)
+        {
+            _logger.LogInformation("Cache hit for {Query}", name);
+            
+            return cachedResult;
+        }
+        
+        _logger.LogInformation("Cache miss for {Query}", name);
+        
+        var result = await next(cancellationToken);
+
+        if (result.IsSuccess)
+        {
+            await _cacheService.SetAsync(request.CacheKey, result, request.Expiration, cancellationToken);
+        }
+        
+        return result;
+    }
+}
--- Bookify.Application/Bookings/Queries/GetBooking/GetBookingQuery.cs
+++ Bookify.Application/Bookings/Queries/GetBooking/GetBookingQuery.cs
@@ -1,6 +1,12 @@
-﻿using Bookify.Application.Abstractions.Messaging;
+﻿using Bookify.Application.Abstractions.Caching;
+using Bookify.Application.Abstractions.Messaging;
 using Bookify.Application.Bookings.Dtos;
 
 namespace Bookify.Application.Bookings.Queries.GetBooking;
 
-public sealed record GetBookingQuery(Guid BookingId): IQuery<BookingResponseDto>;
+public sealed record GetBookingQuery(Guid BookingId) : ICachedQuery<BookingResponseDto>
+{
+    public string CacheKey  => $"{nameof(GetBookingQuery)}-{BookingId}";
+    
+    public TimeSpan? Expiration => null;
+}
